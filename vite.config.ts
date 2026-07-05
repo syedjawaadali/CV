5b70f1a6eecb18302f19c0013a79afc900975403
Syed Jawaad Ali <101031664+syedjawaadali@users.noreply.github.com>
2026-07-05 15:02:48 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/CV/', // Must have leading and trailing slashes
+    base: '/CV/', // Must have leading and trailing slashes for GitHub Pages
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
